939d09b45d2763e58623016c6b3983cc104f147d
Manav Misra <manavm@visionwebsoft.com>
2020-11-05 11:01:45 -0600

    :fire: Remove extraneous code

--- gatsby/src/pages/index.jsx
+++ gatsby/src/pages/index.jsx
@@ -1,9 +1,5 @@
 import React from "react"
 
-const HomePage = () => (
-  <div>
-    <p>Hey! I&apos;m the home page.</p>
-  </div>
-)
+const HomePage = () => <p>Hey! I&apos;m the home page.</p>
 
 export default HomePage
