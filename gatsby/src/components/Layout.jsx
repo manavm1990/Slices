9779d43afbf8680acddc15c08e32369f55433de5
Manav Misra <manavm@visionwebsoft.com>
2020-11-07 07:28:30 -0600

    :heavy_plus_sign: import 'normalize.css'

--- gatsby/src/components/Layout.jsx
+++ gatsby/src/components/Layout.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import Nav from "components/Nav"
 import Footer from "./Footer"
+import "normalize.css"
 
 const Layout = ({ children }) => (
   <>
