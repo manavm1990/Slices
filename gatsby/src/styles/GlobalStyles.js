f397093eebcac200a776dbb33a4e306f4930acb0
Manav Misra <manavm@visionwebsoft.com>
2020-11-07 08:32:29 -0600

    :lipstick: Add 'global typography styles'

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -5,7 +5,13 @@ import { createGlobalStyle } from "styled-components"
 import bg from "assets/bg.svg"
 import stripes from "assets/stripes.svg"
 
+import font from "assets/frenchfries.woff"
+
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: FrenchFries;
+    src: url(${font});
+  }
   :root {
     --color-primary: #FF4649;
     --color-secondary: #ffc600;
@@ -13,16 +19,31 @@ const GlobalStyle = createGlobalStyle`
     --white: #fff;
     --grey: #efefef;
   }
+  html, a {
+    color: var(--black);
+  }
   html {
     background-attachment: fixed;
     background-image: url(${bg});
     background-size: 60rem;
+    font-family: FrenchFries, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 8px;
     scrollbar-color: var(--color-primary) var(--white);
     scrollbar-width: thin;
   }
+  a {
+   text-decoration-color: var(--color-primary);
+   text-decoration-skip-ink: none; 
+  }
   body {
     font-size: 20px;
+  }
+  h1, h2, h3 {
+    font-weight: normal;
+    margin: 0;
+  }
+  p, li {
+    letter-spacing: 1px;
   }
   fieldset {
     border-color: rgba(0,0,0,0.1);
@@ -53,6 +74,16 @@ const GlobalStyle = createGlobalStyle`
   }
   img {
     max-width: 100%
+  }
+  .center {
+    text-align: center;
+  }
+  .mark {
+    background-color: var(--color-secondary);
+    display: inline;
+    line-height: 1;
+    margin: 0;
+    padding: 0 2px 2px 2px;
   }
   .tilt {
     display: inline-block;
