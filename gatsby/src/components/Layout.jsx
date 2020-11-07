72b0f8aa0d6348cf5245d952c0b972d1c390a8ab
Manav Misra <manavm@visionwebsoft.com>
2020-11-07 08:12:49 -0600

    :lipstick: Apply global styles
    :wrench: Ignore imports from styles
    
    :lipstick: Inject global styles

--- gatsby/src/components/Layout.jsx
+++ gatsby/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types"
 import React from "react"
 import Nav from "components/Nav"
+import GlobalStyle from "styles/GlobalStyles"
 import Footer from "./Footer"
 import "normalize.css"
 
 const Layout = ({ children }) => (
   <>
+    <GlobalStyle />
     <Nav />
     {children}
     <Footer />
---
+++ gatsby/src/styles/GlobalStyles.js
@@ -0,0 +1,64 @@
+// Remove the 'local CSS class scoping' for global styles
+import { createGlobalStyle } from "styled-components"
+
+// Gatsby allows for these types of special imports - NOT valid JS!
+import bg from "images/bg.svg"
+import stripes from "images/stripes.svg"
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-primary: #FF4649;
+    --color-secondary: #ffc600;
+    --black: #2E2E2E;
+    --white: #fff;
+    --grey: #efefef;
+  }
+  html {
+    background-attachment: fixed;
+    background-image: url(${bg});
+    background-size: 60rem;
+    font-size: 8px;
+    scrollbar-color: var(--color-primary) var(--white);
+    scrollbar-width: thin;
+  }
+  body {
+    font-size: 20px;
+  }
+  fieldset {
+    border-color: rgba(0,0,0,0.1);
+    border-width: thin;
+  }
+  button, hr {
+    border: 0;
+  }
+  button {
+    --shadow: 2px;
+  
+    background: var(--color-primary);
+    border-radius: 2px;
+    box-shadow: --var(shadow) --var(shadow) 0 var(--grey);
+    color: var(--white);
+    cursor: pointer;
+    padding: 0.5rem 1rem;
+    text-shadow: 1px 1px 0 rgba(0,0,0,0.2);
+    transition: all 0.2s;
+    &:hover {
+      --shadow: 4px;
+    }
+  }
+  hr {
+    background-image: url(${stripes});
+    background-size: 192rem;
+    height: 1rem;
+  }
+  img {
+    max-width: 100%
+  }
+  .tilt {
+    display: inline-block;
+    position: relative;
+    transform: rotate(-2deg);
+  }
+`
+
+export default GlobalStyle
