9237174c8954e729abf131530ccbcb8c84ba7a92
Manav Misra <manavm@visionwebsoft.com>
2020-11-12 09:42:59 -0600

    :construction: Add logo with 'starting styles'

--- gatsby/src/components/Header/Logo/style.js
+++ gatsby/src/components/Header/Logo/style.js
@@ -2,51 +2,37 @@ import styled from "styled-components"
 import stripes from "assets/stripes.svg"
 
 export default styled.div`
-  --borderSize: 1em;
-
   background: white url(${stripes});
-  background-size: 150em;
-  border: var(--borderSize) solid white;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+  background-size: 75rem;
+  border: 0.5rem solid white;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.05);
   display: flex;
 
-  /* This value controls the entire size of the logo */
-  font-size: 6px;
+  /* This value controls the entire size of the logo b/c of 'ems' on all the things. */
   font-size: clamp(1px, 0.65vw, 8px);
   height: 30em;
-  margin-bottom: 0.5rem;
+  margin: 0;
   width: 30em;
+  h1 {
+    align-items: center;
+    display: grid;
+    grid-gap: 2em;
+    grid-row: 2 / span 2;
+    grid-template-rows: 8fr 2fr;
+    margin: 0;
+    transform: translateY(-0.7em);
+  }
+  .est {
+    align-self: center;
+    font-size: 1.5em;
+  }
   .inner {
     align-content: center;
     background: white;
     display: grid;
     flex: 1;
     grid-template-rows: 20% 1fr 1fr;
-    margin: var(--borderSize);
-  }
-  .est {
-    align-self: center;
-    font-size: 1.5em;
-  }
-  h1 {
-    align-items: center;
-    display: grid;
-    grid-gap: 2rem;
-    grid-row: 2 / span 2;
-    grid-template-rows: 8fr 2fr;
-    margin: 0;
-    transform: translateY(-0.7rem);
-  }
-  .slices {
-    font-size: 3.2em;
-    letter-spacing: 0.2rem;
-    transform: translateY(-0.15rem);
-  }
-  .slicks {
-    display: block;
-    perspective: 12.5rem;
-    text-shadow: 0.18rem 0.18rem 0 rgba(0, 0, 0, 0.05);
-    transform: scale(1.4);
+    margin: 1em;
   }
   .letter {
     --rotate: -10deg;
@@ -55,12 +41,12 @@ export default styled.div`
     --translateX: 0;
     --translateY: 0;
 
-    color: var(--red);
+    color: var(--color-primary);
     display: inline-block;
     font-size: 5em;
     line-height: 1;
     transform: scale(var(--scale)) rotate(var(--rotate))
-      translateX(var(--translateX)) translate(var(--translateY))
+      translateX(var(--translateX)) translateY(var(--translateY))
       rotateX(var(--rotateX));
     transition: transform 0.3s;
     &.S {
@@ -96,5 +82,16 @@ export default styled.div`
       --scale: 0.9;
       --translateY: -0.14em;
     }
+  }
+  .slices {
+    font-size: 3.2em;
+    letter-spacing: 0.2em;
+    transform: translateY(-0.15em);
+  }
+  .slicks {
+    display: block;
+    perspective: 100px;
+    text-shadow: 0.18em 0.18em 0 rgba(0, 0, 0, 0.05);
+    transform: scale(1.4);
   }
 `
