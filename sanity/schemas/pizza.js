bdd214a26707968b8b83192e965ba007bc453087
Manav Misra <manavm@visionwebsoft.com>
2020-11-16 14:48:38 -0600

    :construction: Add slug, image and price to 🍕 schema

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -12,5 +12,32 @@ export default {
       type: "string",
       description: "Name of the 🍕",
     },
+    {
+      name: "slug",
+      title: "slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 100,
+      },
+    },
+    {
+      name: "image",
+      title: "Pic",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+      description: "Price of the 🍕 in cents",
+
+      // https://www.sanity.io/docs/validation
+      validation: (Rule) => Rule.min(1000),
+      // TODO: Add custom input component
+    },
   ],
 };
