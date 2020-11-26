e9ec0c0b77cbeb6eab70764bd7b92fca542e926a
Manav Misra <manavm@visionwebsoft.com>
2020-11-26 13:27:15 -0600

    :construction: Get 🍕 data from Sanity :speaker: Log number of pizzas from 'data'

--- gatsby/src/pages/pizzas.jsx
+++ gatsby/src/pages/pizzas.jsx
@@ -1,11 +1,48 @@
+import PropTypes from "prop-types"
 import React from "react"
+import { graphql } from "gatsby"
 
-const PizzasPage = () => (
+const PizzasPage = ({
+  data: {
+    pizzas: { nodes },
+  },
+}) => (
   <p>
     <span role="img" aria-label="pizza">
       🍕
     </span>
+    There are {nodes.length} 🍕s!!!
   </p>
 )
 
+PizzasPage.propTypes = {
+  data: PropTypes.shape({ pizzas: PropTypes.shape }).isRequired,
+}
+
 export default PizzasPage
+
+export const pageQuery = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              # Courtesy of Gatsby for 'everything'
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
