08dbd9aef9674901206f504f73679dc653830903
Manav Misra <manavm@visionwebsoft.com>
2020-12-10 10:56:34 -0600

    :construction: Log 'data' from 'gastby node 🪝'

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,6 +1,33 @@
 // Used to dynamically create pages
 const path = require("path")
 
+const createPizzasPages = async ({ actions, graphql }) => {
+  const {
+    data: {
+      allSanityPizza: { nodes: pizzas },
+    },
+  } = await graphql(`
+    query {
+      allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+  pizzas.forEach(({ name, slug: { current } }) => {
+    console.log(name, current)
+  })
+  return pizzas
+}
+
+exports.createPages = params => {
+  createPizzasPages(params)
+}
+
 // Absolute imports
 exports.onCreateWebpackConfig = ({ _, actions }) => {
   actions.setWebpackConfig({
